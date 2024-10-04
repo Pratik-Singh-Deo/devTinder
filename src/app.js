ecb7c8764a4b526b21c583d732ac3173fa1c57f6
Pratik-Singh-Deo <pratiksinghdeo@gmail.com>
2024-10-04 12:43:32 +0530

    Exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Test router testing!");
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Pratik", lastName: "Singh Deo" });
 });
 
-app.use("/", (req, res) => {
-  res.send("Hello");
+app.post("/user", (req, res) => {
+  // saving data to DB
+  res.send("Data successfully saved to the database!");
 });
 
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully!");
+});
+
+// This will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send("test");
 });
