95d341af90023ed32a5eee4868e682940e727399
Pratik-Singh-Deo <pratiksinghdeo@gmail.com>
2024-10-05 11:52:17 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstname: "Pratik", lastName: "Singh Deo" });
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+//Handle Auth Middleware for all GET POST, .... requests
+app.use("/admin", adminAuth);
+
+app.get("/user/login", (req, res) => {
+  res.send("User logged in successfully");
+});
+
+app.get("/user/data", userAuth, (req, res) => {
+  res.send("User Data Sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data Sent");
 });
 
 app.listen(3000, () => {
